d9cb8a0bfe28bf4423e791d8ce4beef21ba46dd2
Raphael Schwarz <raphael@codeworx.at>
2020-04-22 00:31:37 +0200

    Fix DefaultValue Bug modificationColumn Order Bug

---
+++ test/Bulk.Test/Migrations/20200421211532_AddBoolFlag.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bulk.Test.Migrations
+{
+    public partial class AddBoolFlag : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModificationDate",
+                table: "SimpleTableWithShadowProperty",
+                nullable: true,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldNullable: true,
+                oldDefaultValueSql: "getdate()");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "BoolFlag",
+                table: "SimpleTableWithShadowProperty",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BoolFlag",
+                table: "SimpleTableWithShadowProperty");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModificationDate",
+                table: "SimpleTableWithShadowProperty",
+                nullable: true,
+                defaultValueSql: "getdate()",
+                oldClrType: typeof(DateTime),
+                oldNullable: true,
+                oldDefaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
