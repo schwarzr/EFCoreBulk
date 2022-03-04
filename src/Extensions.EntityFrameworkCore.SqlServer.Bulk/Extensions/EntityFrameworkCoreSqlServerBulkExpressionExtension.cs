a5aaf982543da8445ac1ebe669ef22a86f9ddfe4
Raphael Schwarz <raphael@codeworx.at>
2022-03-04 15:01:14 +0100

    #7 Support properties with HasConversion expression

---
+++ src/Extensions.EntityFrameworkCore.SqlServer.Bulk/Extensions/EntityFrameworkCoreSqlServerBulkExpressionExtension.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Linq.Expressions
+{
+    public static class EntityFrameworkCoreSqlServerBulkExpressionExtension
+    {
+        public static Expression Replace(this Expression baseExpression, Expression search, Expression replace)
+        {
+            var visitor = new ReplaceExpressionVisitor(search, replace);
+
+            return visitor.Visit(baseExpression);
+        }
+
+        private class ReplaceExpressionVisitor : ExpressionVisitor
+        {
+            private Expression _search;
+            private Expression _replace;
+
+            public ReplaceExpressionVisitor(Expression search, Expression replace)
+            {
+                _search = search;
+                _replace = replace;
+            }
+
+            public override Expression Visit(Expression node)
+            {
+                if (node == _search)
+                {
+                    return _replace;
+                }
+                return base.Visit(node);
+            }
+        }
+    }
+}
